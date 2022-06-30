ad4e02351755110ce19403c49bc270e463a2da2b
Invincible <571574085@qq.com>
2022-06-30 04:54:12 +0000

    跑起来

--- src/App.vue
+++ src/App.vue
@@ -1,127 +1,106 @@
 <template>
-  <a-form :form="form" @submit="handleSubmit">
-    <a-form-item
-      v-for="item of form.getFieldValue('keys')"
-      :key="item"
-      v-bind="formItemLayout"
-      :label="item"
+  <a-table
+    :columns="columns"
+    :data-source="data"
+    class="components-table-demo-nested"
+  >
+    <div slot="expandIcon">
+      dasf1243asdfsadf
+    </div>
+    <div slot="title">
+      hdfhfg
+    </div>
+    <div slot="footer">
+      zxvcvxcz
+    </div>
+    <a slot="operation" slot-scope="text">Publish</a>
+    <a-table
+      slot="expandedRowRender"
+      slot-scope="text"
+      :columns="innerColumns"
+      :data-source="innerData"
+      :pagination="false"
     >
-      {{item}}
-      <a-input
-        v-decorator="[
-          `names['${item}']`,
-          {
-            validateTrigger: ['change', 'blur'],
-            rules: [{
-              required: true,
-              whitespace: true,
-              message: 'Please input passenger\'s name or delete this field.',
-            }],
-          }
-        ]"
-        placeholder="passenger name"
-        style="width: 60%; margin-right: 8px"
-      />
-      <a-icon
-        v-if="form.getFieldValue('keys').length > 1"
-        class="dynamic-delete-button"
-        type="minus-circle-o"
-        :disabled="form.getFieldValue('keys').length === 1"
-        @click="() => remove(item)"
-      />
-    </a-form-item>
-    <a-form-item v-bind="formItemLayoutWithOutLabel">
-      <a-button type="dashed" style="width: 60%" @click="add">
-        <a-icon type="plus"/>Add field
-      </a-button>
-    </a-form-item>
-    <a-form-item v-bind="formItemLayoutWithOutLabel">
-      <a-button type="primary" html-type="submit">Submit</a-button>
-    </a-form-item>
-  </a-form>
+      <span slot="status" slot-scope="text">
+        <a-badge status="success" />Finished
+      </span>
+      <span slot="operation" slot-scope="text" class="table-operation">
+        <a>Pause</a>
+        <a>Stop</a>
+        <a-dropdown>
+          <a-menu slot="overlay">
+            <a-menu-item>
+              Action 1
+            </a-menu-item>
+            <a-menu-item>
+              Action 2
+            </a-menu-item>
+          </a-menu>
+          <a> More <a-icon type="down" /> </a>
+        </a-dropdown>
+      </span>
+    </a-table>
+  </a-table>
 </template>
-
 <script>
-let arr = ["a", "b", "v", "d"];
+const columns = [
+  { title: "Name", dataIndex: "name", key: "name" },
+  { title: "Platform", dataIndex: "platform", key: "platform" },
+  { title: "Version", dataIndex: "version", key: "version" },
+  { title: "Upgraded", dataIndex: "upgradeNum", key: "upgradeNum" },
+  { title: "Creator", dataIndex: "creator", key: "creator" },
+  { title: "Date", dataIndex: "createdAt", key: "createdAt" },
+  {
+    title: "Action",
+    key: "operation",
+    scopedSlots: { customRender: "operation" }
+  }
+];
+
+const data = [];
+for (let i = 0; i < 3; ++i) {
+  data.push({
+    key: i,
+    name: "Screem",
+    platform: "iOS",
+    version: "10.3.4.5654",
+    upgradeNum: 500,
+    creator: "Jack",
+    createdAt: "2014-12-24 23:12:00"
+  });
+}
+
+const innerColumns = [
+  { title: "Date", dataIndex: "date", key: "date" },
+  { title: "Name", dataIndex: "name", key: "name" },
+  { title: "Status", key: "state", scopedSlots: { customRender: "status" } },
+  { title: "Upgrade Status", dataIndex: "upgradeNum", key: "upgradeNum" },
+  {
+    title: "Action",
+    dataIndex: "operation",
+    key: "operation",
+    scopedSlots: { customRender: "operation" }
+  }
+];
+
+const innerData = [];
+for (let i = 0; i < 3; ++i) {
+  innerData.push({
+    key: i,
+    date: "2014-12-24 23:12:00",
+    name: "This is production name",
+    upgradeNum: "Upgraded: 56"
+  });
+}
+
 export default {
   data() {
     return {
-      formItemLayout: {
-        labelCol: {
-          xs: { span: 24 },
-          sm: { span: 4 }
-        },
-        wrapperCol: {
-          xs: { span: 24 },
-          sm: { span: 20 }
-        }
-      },
-      formItemLayoutWithOutLabel: {
-        wrapperCol: {
-          xs: { span: 24, offset: 0 },
-          sm: { span: 20, offset: 4 }
-        }
-      }
+      data,
+      columns,
+      innerColumns,
+      innerData
     };
-  },
-  beforeCreate() {
-    this.form = this.$form.createForm(this);
-    this.form.getFieldDecorator("keys", { initialValue: [], preserve: true });
-  },
-  methods: {
-    remove(k) {
-      const { form } = this;
-      // can use data-binding to get
-      const keys = form.getFieldValue("keys");
-      // We need at least one passenger
-      if (keys.length === 1) {
-        return;
-      }
-
-      // can use data-binding to set
-      form.setFieldsValue({
-        keys: keys.filter(key => key !== k)
-      });
-    },
-
-    add() {
-      const { form } = this;
-      // can use data-binding to get
-      const keys = form.getFieldValue("keys");
-      const nextKeys = keys.concat(arr[keys.length]);
-      // can use data-binding to set
-      // important! notify form to detect changes
-      form.setFieldsValue({
-        keys: nextKeys
-      });
-      this.$nextTick(() => console.log(form.getFieldsValue()));
-    },
-
-    handleSubmit(e) {
-      e.preventDefault();
-      this.form.validateFields((err, values) => {
-        if (!err) {
-          console.log("Received values of form: ", values);
-        }
-      });
-    }
   }
 };
 </script>
-<style>
-.dynamic-delete-button {
-  cursor: pointer;
-  position: relative;
-  top: 4px;
-  font-size: 24px;
-  color: #999;
-  transition: all 0.3s;
-}
-.dynamic-delete-button:hover {
-  color: #777;
-}
-.dynamic-delete-button[disabled] {
-  cursor: not-allowed;
-  opacity: 0.5;
-}
-</style>
